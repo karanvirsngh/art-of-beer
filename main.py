925d0ca449058f8e6e7aa1dbfa670cef877e771a
CVBaqq <ran2356@gmail.com>
2013-04-24 13:22:59 -0600

    added screen management (transitions)

--- main.py
+++ main.py
@@ -5,42 +5,101 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.properties import NumericProperty
 from kivy.uix.widget import Widget
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
 #from kivy.interactive import InteractiveLauncher
 
-import mysql.connector
+#import mysql.connector
 
-cnx = mysql.connector.connect(user='karanvirsngh', password='ether0110',
-                              host='SQL09.FREEMYSQL.NET',
-                              database='karant')
+#cnx = mysql.connector.connect(user='karanvirsngh', password='ether0110',
+                              #host='SQL09.FREEMYSQL.NET',
+                              #database='karant')
 
-cursor = cnx.cursor()
+#cursor = cnx.cursor()
 
-cursor.execute("SELECT * FROM Writers")
-karanvir="oi"
-print cursor
-for Name in cursor:
-    print Name[1]
-    karanvir=Name[1]
+#cursor.execute("SELECT * FROM Writers")
+#karanvir="oi"
+#print cursor
+#for Name in cursor:
+#    print Name[1]
+#    karanvir=Name[1]
 
-class ProductDetailScreen(Widget):
-    pass
+#sm.add_widget(ProductDetailScreen(name='Product_Detail_Screen'))
+#sm.add_widget(ProductFilterScreen(name='Product_Filter_Screen'))
+#sm.current = 'Main_Screen'
+# Create the manager
+#sm = ScreenManager()
+# Add few screens
+#for i in xrange(4):
+ #   screen = Screen(name='Title %d' % i)
+ #   sm.add_widget(screen)
 
-class ProductScreen(Widget):
-    pass
+# By default, the first screen added into the ScreenManager will be
+# displayed. Then, you can change to another screen:
 
-class ProductFilterScreen(Widget):
-    pass
+# Let's display the screen named 'Title 2'
+# The transition will be automatically used.
+#sm.current = 'Title 2'
+Builder.load_string('''
+#:kivy 1.0.9
 
-class MainScreen(Widget):
-    def show_product_screen(self):
-        return ProductScreen()
+<MainScreen>:
+    FloatLayout:
+        size: root.width, root.height
+        canvas:
+            Color:
+                rgb: 1, 1, 1
+            Rectangle:
+                source: 'images/main_background.jpg'
+                size: root.size
+        
+        Button:
+            color: (0, 0, 0, 1)
+            italic: True
+            text: 'Start Exporing Art of Beer'
+            font_size: '20sp'
+            background_color: (1, 1, 1, .3)
+            color: (0, 0, 0, 1)
+            on_press: root.on_click()
+           
+<ProductScreen>:
+    FloatLayout:
+        size: root.width, root.height
+        canvas:
+            Color:
+                rgb: 0, 0, 1
+            Rectangle:
+                size: root.size
+        Button:
+            text: 'Go back'
+            size_hint: None, None
+            size: 150, 50
+            on_press: root.on_click()
+''')
+class ProductScreen(Screen):
+    def on_click(self):
+        sm.current = 'Main_Screen'
+        #sm.transition = SlideTransition(direction="up")
 
-    def on_pause(self):
-        return True
+
+#class ProductDetailScreen(Screen):
+#    pass
+
+#class ProductFilterScreen(Screen):
+#    pass
+
+class MainScreen(Screen):
+    def on_click(self):
+        sm.current = 'Product_Screen'
+# Create the screen manager
+sm = ScreenManager()
+sm.add_widget(MainScreen(name='Main_Screen'))
+sm.add_widget(ProductScreen(name='Product_Screen'))
+#sm.add_widget(Product_Detail_Screen(name='Product_Detail_Screen'))
+#sm.add_widget(ProductFilterScreen(name='ProductFilterScreen'))
 
 class ArtofBeerApp(App):
     def build(self):
-        return MainScreen()
+        return sm
 
 if __name__ == '__main__':
     ArtofBeerApp().run()
